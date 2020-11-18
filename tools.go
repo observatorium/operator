2d72f4c6006041ad8e171c1fe838992a7d860f69
Kemal Akkoyun <kakkoyun@gmail.com>
2020-11-18 18:34:51 +0100

    Use bingo for tools
    Signed-off-by: Kemal Akkoyun <kakkoyun@gmail.com>

--- tools.go
+++ tools.go
@@ -3,8 +3,7 @@
 package main
 
 import (
-	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 	_ "github.com/brancz/locutus"
-	_ "github.com/jsonnet-bundler/jsonnet-bundler/cmd/jb"
 	_ "github.com/observatorium/observatorium/test/tls"
+	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
 )
